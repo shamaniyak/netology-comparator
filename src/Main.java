3795ae819c863e7f961e7d2a36fd7b5d85949e01
Shamaniyak <shamaniyak@ya.ru>
2022-09-14 00:21:36 +0300

    eraser

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,19 @@
-import java.util.Collections;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args) {
         List<Person> persons = new ArrayList<>();
         persons.add(new Person("Семен", "Слепаков", 30));
-        persons.add(new Person("Вася", "Бубун Чума", 22));
+        persons.add(new Person("Вася", "Бубун Чума", 16));
         persons.add(new Person("Коля", "Иванов", 50));
         persons.add(new Person("Петя", "Пупкин Кудрявый Младший", 40));
+        persons.add(new Person("Frenk", "Havi Mettal", 12));
 
-        Collections.sort(persons, new PersonComparator(2));
+        persons.sort(new PersonComparator(2));
+        Predicate<Person> predicate = (Person p) -> p.getAge() < 18;
+        persons.removeIf(predicate);
 
         System.out.println(persons);
     }
